874d5e174b9e7e5d141c9b421e6c8f1352682d86
Gudela yaswanth sai kumar <79440730+yaswanthsk@users.noreply.github.com>
2025-04-26 23:48:53 +0530

    Update JobSeekerNavbar.js

---
+++ frontend/src/JobSeeker/JobSeekerNavbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link, useNavigate  } from 'react-router-dom';
+import './JobSeekerLanding'; // Reusing existing CSS class names
+
+const JobSeekerNavbar = () => {
+
+  const navigate = useNavigate(); // Hook to navigate programmatically
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/login'); 
+  };
+
+  return (
+    <nav className="SavedJobs-navbar">
+      <div className="SavedJobs-logo">Jobify</div>
+      <ul className="SavedJobs-nav-links">
+        <li><Link to="/jobseeker/dashboard">Browse Jobs</Link></li>
+        <li><Link to="/jobseeker/savedjob" className="SavedJobs-link">Saved</Link></li>
+        <li><Link to="/jobseeker/my-applications" className="SavedJobs-link">My Applications</Link></li>
+        <li><Link to="/jobseeker/profile">Profile</Link></li>
+        <li><button onClick={handleLogout} className="SavedJobs-logoutBtn">Logout</button></li>
+
+      </ul>
+    </nav>
+  );
+};
+
+export default JobSeekerNavbar;
